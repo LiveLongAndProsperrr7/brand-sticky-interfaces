4adb048411db3fc58b2f8d586cd7c01593a60659
LiveLongAndProsperrr7 <157714423+LiveLongAndProsperrr7@users.noreply.github.com>
2024-03-11 01:16:49 +0100

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,26 @@
+    data = [random.randint(1, 100) for _ in range(10)]
+
+    data = generate_random_data()
+
+    return data
+        print(f"Random Number: {item}")
+
+def main():
+
+    for item in data:
+    main()
+import random
+if __name__ == "__main__":
+def generate_random_data():def main():
+
+    main()
+    return data
+
+    data = [random.randint(1, 100) for _ in range(10)]
+    for item in data:
+if __name__ == "__main__":
+    data = generate_random_data()
+        print(f"Random Number: {item}")
+def generate_random_data():
+
+import random
